9469b1556265c1c019323f432c0f13c5ab5b2605
redflameken2 <202301178@iacademy.edu.ph>
2025-04-13 16:20:15 +0800

    initial commit (prolly should've commited much earlier)

---
+++ src/scripts/input.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+    * @param {string} element_id 
+    */
+function element_gain_focus(element_id){
+    const element = document.getElementsByName(element_id)[0]
+    if(element != null){
+        element.focus;
+    }
+}
+
